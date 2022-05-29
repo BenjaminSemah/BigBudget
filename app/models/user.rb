d44a04818a21d48634cbf882d04de214a44c0340
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-29 09:37:02 +0000

    Create models with validations according to ER diagram

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :entities
+  has_many :groups
 end
--- db/migrate/20220529085532_create_groups.rb
+++ db/migrate/20220529085532_create_groups.rb
@@ -2,6 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name
+      t.string :icon
 
       t.timestamps
     end
