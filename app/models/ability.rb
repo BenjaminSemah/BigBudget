f644f8b1c8fd8d1f75ecfe94f1cc1c6752d18754
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-30 09:58:50 +0000

    Implement authorization with cancancan gem

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :manage, Recipe do |recipe|
+      recipe.user_id == user.id
+    end
+  end
+end
