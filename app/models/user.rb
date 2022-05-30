b2ae0944b592c8f00297c49cf010ba892bca08ff
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-30 06:27:08 +0000

    Add Sign Up and Login pages using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # rubocop:disable Layout/EndOfLine
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
 
   has_many :entities
