b02377c560398fa5455fcd70f7f94f9d68a2335a
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-31 15:42:09 +0000

    Implement transactions page

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,51 +1,35 @@
 class EntitiesController < ApplicationController
   before_action :set_entity, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
-  # GET /entities or /entities.json
   def index
-    @entities = Entity.all
+    @group = Group.find(params[:group_id])
+    @entities = @group.entities.order(created_at: :desc)
+
+    redirect_to new_user_session_path, notice: 'You cannot access this resource.' if @group.user != current_user
   end
 
-  # GET /entities/1 or /entities/1.json
-  def show; end
-
-  # GET /entities/new
   def new
+    @group = Group.find(params[:group_id])
+    @groups = Group.where(user: current_user)
     @entity = Entity.new
   end
 
-  # GET /entities/1/edit
   def edit; end
 
-  # POST /entities or /entities.json
   def create
     @entity = Entity.new(entity_params)
+    @entity.user = current_user
+    @group = Group.find(params[:group_id])
+    @group.entities << @entity
 
-    respond_to do |format|
-      if @entity.save
-        format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully created.' }
-        format.json { render :show, status: :created, location: @entity }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
-      end
+    if @entity.save
+      redirect_to group_entities_path(@group), notice: 'Your transaction has been added successfully!'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /entities/1 or /entities/1.json
-  def update
-    respond_to do |format|
-      if @entity.update(entity_params)
-        format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully updated.' }
-        format.json { render :show, status: :ok, location: @entity }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /entities/1 or /entities/1.json
   def destroy
     @entity.destroy
 
@@ -64,6 +48,6 @@ class EntitiesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def entity_params
-    params.require(:entity).permit(:name)
+    params.require(:entity).permit(:name, :amount, :user_id)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,15 @@ class Group < ApplicationRecord
   validates :icon, presence: true
 
   belongs_to :user
-  has_many :entities
+  has_many :group_entities
+  has_many :entities, through: :group_entities
+
+  def total_amount
+    total = 0
+    entities.each do |entity|
+      total += entity.amount
+    end
+    total
+  end
 end
 # rubocop:enable Layout/EndOfLine
