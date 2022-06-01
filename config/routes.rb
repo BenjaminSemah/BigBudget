885b68fd25e5004f1517abf8169ef59ccf917f64
Benjamin Semah <benjaminsemah@gmail.com>
2022-06-01 10:10:12 +0000

    Fix issues with logout button

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,6 @@ Rails.application.routes.draw do
     resources :entities
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "splash_screen#index"
-
-  ###########################
-
   unauthenticated do
     root "splash_screen#index"
   end
@@ -19,8 +12,6 @@ Rails.application.routes.draw do
   authenticated :user do
     root 'groups#index', as: :authenticated_root
   end
-
-  ###########################
 end
 
 
